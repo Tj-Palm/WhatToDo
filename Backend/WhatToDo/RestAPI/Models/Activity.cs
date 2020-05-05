dfbc5aeea848b884da62ade1236c870d5dd7239d
Benjamin Wieghorst Sørensen <benj448c@edu.easj.dk>
2020-05-05 13:50:06 +0200

    added again

---
+++ Backend/WhatToDo/RestAPI/Models/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace RestApi.Models
+{
+    public class Activity
+    {
+        [Key]
+        public int id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan TimeUsage { get; set; }
+        public string Type { get; set; }
+        public bool Outdoor { get; set; }
+    }
+}

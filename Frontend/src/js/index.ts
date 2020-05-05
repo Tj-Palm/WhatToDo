a036d19e020688cd2c13c6de4653a0157a287abb
Tj-Palm <48101800+Tj-Palm@users.noreply.github.com>
2020-05-05 10:35:21 +0200

    Opstart frontend
    Co-authored-by: Sarah Katrine Minjon Waest <sara693f@edu.easj.dk>

--- Frontend/src/js/index.ts
+++ Frontend/src/js/index.ts
@@ -1,12 +1,109 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import Axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index";
+
+import {json2table100} from "./genericTable";
+
+let BaseUri: string = "http://anbo-bookstorerest.azurewebsites.net/api/books"
+let AllBooks: JSON;
+
+interface IActivity {
+    id: number,
+    name: string,
+    type: string,
+    time: number,
+    weather: string
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+interface IBook {
+    id: number,
+    title: string,
+    author: string,
+    publisher: string,
+    price: number,
+ }
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+new Vue({
+    el: "#App",
+    data: {
+        // activities: [],
+        books: [],
+        errors: [],
+        deleteId: 0,
+        deleteMessage: "",
+        // formData: { name: "", type: "", weather: "", time: 0 },
+        formData: { title: "", author: "", publisher: "", price: 0 },
+        addMessage: ""
+    },
+    created(){
+        // this.getAllActivities(),
+        this.getAllBooks(),
+        this.getAllBooksJSON()
+    },
+    methods: {
+        getAllActivities() {
+            Axios.get<IActivity[]>(BaseUri + "activities") 
+                .then((response: AxiosResponse<IActivity[]>) => {
+                    this.activities = response.data
+                    console.log("Activities then")
+                })
+                .catch((error: AxiosError) => {
+                    console.log("Activities catch")
+                    //this.message = error.message
+                    alert(error.message) // https://www.w3schools.com/js/js_popup.asp
+                })
+        },
+        getAllBooks() {
+            Axios.get<IBook[]>(BaseUri) 
+                .then((response: AxiosResponse<IBook[]>) => {
+                    this.books = response.data
+                    console.log("Book then")
+                })
+                .catch((error: AxiosError) => {
+                    //this.message = error.message
+                    console.log("Book catch")
+                    alert(error.message) // https://www.w3schools.com/js/js_popup.asp
+                })
+        },
+        getAllBooksJSON() {
+            Axios.get(BaseUri)
+            .then((Response: AxiosResponse): void =>{
+                console.log("Get books")
+                let data: IBook[] = Response.data;
+                console.log(data)
+                let result: string = json2table100(data)
+                console.log(result)
+            })
+        }
+        // deleteActivity(deleteId: number) {
+        //     let uri: string = BaseUri + "activities" + "/" + deleteId
+        //     Axios.delete<void>(uri)
+        //         .then((response: AxiosResponse<void>) => {
+        //             this.deleteMessage = "Activity Deleted"
+        //             this.getAllActivities()
+        //         })
+        //         .catch((error: AxiosError) => {
+        //             //this.deleteMessage = error.message
+        //             alert(error.message)
+        //         })
+        // },
+        // addActivity() {
+        //     let name : HTMLInputElement = <HTMLInputElement> document.getElementById("inputname");
+        //     let type : HTMLInputElement = <HTMLInputElement> document.getElementById("inputtype");
+        //     let weather : HTMLInputElement = <HTMLInputElement> document.getElementById("inputweather");
+        //     let time : HTMLInputElement = <HTMLInputElement> document.getElementById("inputtime");
+
+        //     Axios.post<IActivity>(BaseUri + "activities", this.formData)
+        //         .then((response: AxiosResponse) => {
+        //             let message: string = "Activity Added"
+        //             this.addMessage = message
+        //             this.getAllActivities()
+        //         })
+        //         .catch((error: AxiosError) => {
+        //             // this.addMessage = error.message
+        //             alert(error.message)
+        //         })
+        // }
+    }
+})

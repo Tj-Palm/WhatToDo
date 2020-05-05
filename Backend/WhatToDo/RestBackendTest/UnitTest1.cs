edb7a61711a1a80ce9a1545a198d97e2b34e431b
Nikolaj Dufke <nikolajdufke@live.dk>
2020-05-05 10:22:24 +0200

    unit test get all activity class

--- Backend/WhatToDo/RestBackendTest/UnitTest1.cs
+++ Backend/WhatToDo/RestBackendTest/UnitTest1.cs
@@ -5,9 +5,34 @@ namespace RestBackendTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private ActivityController _activityController;
+
+        [TestInitialize]
+        public void init()
         {
+            _actionController = new ActivityController();
         }
+
+        [TestMethod]
+        public void GetAll()
+        {
+
+            //ARRANGE
+            //kan ændre sig i forhold til antallet af test data i databasen.
+             int expected = 8;
+
+            //ASSERT
+            Assert.AreEqual(expected, _activityController.get().count);
+
+        }
+
+        [TestMethod]
+        public void GetTimeOfAnActivity()
+        {
+
+        }
+
+
+
     }
 }

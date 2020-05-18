9e19587e74315d88f08a6f5820cef8f8b79d2407
SarahWaest <48202579+SarahWaest@users.noreply.github.com>
2020-05-18 13:45:19 +0200

    Added a measurement class, UPD in program.. It's all in the new sensor project

---
+++ Backend/WhatToDo/Sensor/Measurement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sensor
+{
+    public class Measurement
+    {
+        public DateTime MeasureTime { get; set; }
+        public string Compound { get; set; }
+        public double MeasurementValue { get; set; }
+
+
+        public Measurement(DateTime measureTime, double measurementValue )
+        {
+            MeasureTime = measureTime;
+            MeasurementValue = measurementValue;
+        }
+
+        public override string ToString()
+        {
+            return MeasureTime + " " + Compound + " " + MeasurementValue;
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+    }
+}
---
+++ Backend/WhatToDo/Sensor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+
+namespace Sensor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World press enter to start prepare sending environment!");
+            Console.ReadLine();
+            Random rvalue = new Random();
+            Measurement measure;
+
+            UdpClient udpSender = new UdpClient(0);
+            udpSender.EnableBroadcast = true;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 1111);
+
+            Console.WriteLine("ready to start press enter again");
+            Console.ReadLine();
+
+            while (true)
+            {
+                measure = new Measurement(DateTime.Now,rvalue.Next(10));
+
+                byte[] sendBytes = Encoding.ASCII.GetBytes(measure.ToString());
+
+                try
+                {
+                    Console.WriteLine("sending :" + measure.ToString());
+                    udpSender.Send(sendBytes, sendBytes.Length, endPoint);
+                    Thread.Sleep(10000);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+    }
+}
